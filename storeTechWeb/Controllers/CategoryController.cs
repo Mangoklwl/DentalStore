9095096752ced912249e80d21e9f3eea6dc2ba97
Ricardo <hermit.4493@gmail.com>
2025-11-10 12:34:27 +0100

    feat: add upsert in controller

--- storeTechWeb/Controllers/CategoryController.cs
+++ storeTechWeb/Controllers/CategoryController.cs
@@ -1,21 +1,38 @@
 ﻿using DataBaseAccess;
 using Microsoft.AspNetCore.Mvc;
+using ModelClasses;
 
 namespace storeTechWeb.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : Controller   // Declara una clase pública llamada CategoryController que hereda de la clase base 'Controller' de ASP.NET MVC.
     {
-        private readonly ApplicationDBContext _context;
+        private readonly ApplicationDBContext _context;  // Declara una variable privada y de solo lectura llamada _context, que almacenará una instancia del contexto de base de datos.
 
-        public CategoryController(ApplicationDBContext context)
+        public CategoryController(ApplicationDBContext context)   // Constructor del controlador, que recibe como parámetro el contexto de la base de datos (inyectado por dependencia).
         {
-            _context = context;
+            _context = context;   // Asigna el contexto recibido al campo privado _context, para poder usarlo en los métodos del controlador.
         }
 
-        public IActionResult Index()
+        public IActionResult Index()   // Define una acción pública llamada Index que devuelve un IActionResult (un tipo genérico de resultado en MVC).
         {
-            var items=_context.categories.ToList();
-            return View(items);
+            
+            var items = _context.categories.ToList();   // Obtiene todos los registros de la tabla 'categories' desde la base de datos y los convierte en una lista.
+            return View(items);   // Devuelve la vista asociada al método (por convención, "Index.cshtml") y le pasa la lista de categorías como modelo.
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if(id == 0 )
+            {
+                Category category = new Category();
+                return View (category);
+            }
+            else
+            {
+                var category = _context.categories.FirstOrDefault(u=> u.Id==id);
+                return View(category);
+            }
         }
     }
+
 }
